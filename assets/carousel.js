9e0a81326c78c5cb20018de5d15a8d94eee6134e
Grzegorz Golec <grzegorz.golec.squareboxmobile@gmail.com>
2026-01-08 19:07:40 +0100

    add splidejs lib to handle carousels

---
+++ assets/carousel.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  if (!Splide) {
+    return;
+  }
+
+  const splide = new Splide('.splide', {
+    arrows: false,
+    gap: '40px',
+    lazyLoad: true,
+    padding: { left: '5%', right: '10%' },
+    pagination: false,
+    perPage: 5,
+    rewind : false,
+    type: 'loop',
+
+    breakpoints: {
+      1200: {
+        perPage: 2, gap: 0, padding: { left: '0%', right: '20%' },
+      },
+    },
+  });
+  splide.mount();
+});
